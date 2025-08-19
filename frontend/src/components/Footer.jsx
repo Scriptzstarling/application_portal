aa55d256b0f59a33bbe1f2607074e56a860b5d54
Scriptzstarling <shashwat11muz@gmail.com>
2025-08-19 14:54:03 +0530

    update

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,12 +5,12 @@ export default function Footer() {
   
   return (
     <footer className="w-full bg-slate-50 border-t border-slate-200 text-slate-700 px-4 py-5">
-      <div className="flex justify-between items-center text-sm">
-        <div className="flex-1">
+      <div className="flex flex-col sm:flex-row justify-between items-center text-sm gap-2 sm:gap-0">
+        <div className="flex-1 text-center sm:text-left">
           Copyright © {currentYear} [MSSY].
         </div>
         
-        <div className="flex-1 text-right">
+        <div className="flex-1 text-center sm:text-right">
           Mukhyamantri Shram Shakti Yojna (मुख्यमंत्री श्रम शक्ति योजना).
         </div>
       </div>
